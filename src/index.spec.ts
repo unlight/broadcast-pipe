74ec35b9f4ef3a873f7a53e6aa7c6b0779539e2f
roman <root@localhost>
2018-08-08 18:18:22 +0400

    react life cycle dcorarator

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,10 +1,47 @@
+/* eslint-disable import/no-duplicates, tslint/config */
 import * as assert from 'assert';
+import { on, emit, reactLifeCyclable } from './index';
 import * as lib from './index';
+import { injector } from 'njct';
+
+class MockBroadcastChannel implements BroadcastChannel {
+
+    constructor(public readonly name: string) { }
+    onmessage() { }
+    onmessageerror() { }
+    close() { }
+    postMessage() { }
+    addEventListener() { }
+    removeEventListener() { }
+    dispatchEvent() {
+        return true;
+    }
+}
+
+injector.mock('BroadcastChannel', () => MockBroadcastChannel);
 
 it('smoke', () => {
     assert(lib);
 });
 
-it('decorators', () => {
+it('on regular property', () => {
+    class C {
+        @on('event')
+        property;
+    }
+    const c = new C();
+    emit('event', 42);
+    assert.strictEqual(c.property, 42);
+});
 
+
+it('reactLifeCyclable decorator dummy', () => {
+    @reactLifeCyclable()
+    class Component {
+        @on('event')
+        property;
+    }
+    const c = new Component();
+    emit('event', 42);
+    assert.strictEqual(c.property, 42);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import { inject } from 'njct';
+
+const BroadcastChannelCtor = inject('BroadcastChannel', () => BroadcastChannel);
 const channels: { [name: string]: BroadcastChannel } = {};
 
 function channel(name: string) {
     if (!channels[name]) {
-        channels[name] = new BroadcastChannel(name);
+        channels[name] = new BroadcastChannelCtor(name);
     }
     return channels[name];
 }
@@ -13,6 +16,7 @@ export function on(name: string) {
         value = e.data;
     });
     return function(target: any, property: string | symbol, descriptor: PropertyDescriptor) {
+        // todo: handle old property
         descriptor.get = () => {
             return value;
         };
@@ -26,4 +30,19 @@ export function emit<T = any>(name: string, data: T) {
 export function off(name: string) {
     channel(name).close();
     delete channels[name];
+}
+
+export function reactLifeCyclable(): ClassDecorator {
+    return function(target: any) {
+        const componentDidMount = target.componentDidMount;
+        const componentWillUnMount = target.componentWillUnMount;
+
+        target.componentDidMount = function() {
+            componentDidMount.apply(this, arguments);
+        };
+
+        target.componentWillUnmount = function() {
+            componentWillUnMount.apply(this, arguments);
+        };
+    };
 }
